86cf642dcfdb3906fbb5eda966763a0faed6aa1e
Jeff Nyman <jeffnyman@gmail.com>
2020-05-07 13:44:08 -0500

    Refactor of Excavation.

--- app/models/excavation.rb
+++ app/models/excavation.rb
@@ -1,13 +1,11 @@
 class Excavation
+  attr_accessor :activities
+
   def initialize
     @activities = []
   end
 
-  def activities
-    @activities
-  end
-
   def finished?
-    activities.length == 0
+    activities.empty?
   end
 end
