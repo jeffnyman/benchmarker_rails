50585359ae1369697cbf5ae74690cb6086826bcd
Jeff Nyman <jeffnyman@gmail.com>
2020-05-08 09:11:20 -0500

    Start of first system test.

--- spec/support/system.rb
+++ spec/support/system.rb
@@ -1,5 +1,5 @@
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    drive_by :rack_test
+    driven_by :rack_test
   end
 end
---
+++ spec/system/add_excavation_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe "Excavations", type: :system do
+  it "users can create an excavation with activities" do
+    visit new_excavation_path
+
+    fill_in "Name", with: "Talpiot Tomb"
+    fill_in "Activities", with: "Expose antechamber:3\nRemove terra rossa soil:5"
+    click_on("Create Excavation")
+
+    visit excavations_path
+
+    expect(page).to have_content("Talpiot Tomb")
+    expect(page).to have_content(8)
+  end
+end
