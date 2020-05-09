daeb414d2786c21cc272cc831cf8f3adabb02809
Jeff Nyman <jeffnyman@gmail.com>
2020-05-09 02:25:58 -0500

    System test is passing.

--- app/controllers/excavations_controller.rb
+++ app/controllers/excavations_controller.rb
@@ -2,4 +2,18 @@ class ExcavationsController < ApplicationController
   def new
     @excavation = Excavation.new
   end
+
+  def create
+    @workflow = CreatesExcavation.new(
+      name: params[:excavation][:name],
+      activity_string: params[:excavation][:activities]
+    )
+
+    @workflow.create
+    redirect_to excavations_path
+  end
+
+  def index
+    @excavations = Excavation.all
+  end
 end
